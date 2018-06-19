712ca109840734567e7e8f4e4a0c78e3f994527f
mmmaaaggg <mmmaaaggg@163.com>
2018-06-19 19:45:28 +0800

    add min60 daily table

--- backend/orm.py
+++ backend/orm.py
@@ -64,24 +64,101 @@ class MDMin1(BaseModel):
     count = Column(DOUBLE)
 
 
+class MDMin1FIllHistory(BaseModel):
+    __tablename__ = 'md_min1_bc_fill_history'
+    market = Column(String(10), primary_key=True)
+    pair = Column(String(10), primary_key=True)
+    ts_start = Column(TIMESTAMP, primary_key=True)
+    ts_curr = Column(TIMESTAMP)
+    open = Column(DOUBLE)
+    high = Column(DOUBLE)
+    low = Column(DOUBLE)
+    close = Column(DOUBLE)
+    amount = Column(DOUBLE)
+    vol = Column(DOUBLE)
+    count = Column(DOUBLE)
+
+
+class MDMin60(BaseModel):
+    __tablename__ = 'md_min60_bc'
+    market = Column(String(10), primary_key=True)
+    pair = Column(String(10), primary_key=True)
+    ts_start = Column(TIMESTAMP, primary_key=True)
+    ts_curr = Column(TIMESTAMP)
+    open = Column(DOUBLE)
+    high = Column(DOUBLE)
+    low = Column(DOUBLE)
+    close = Column(DOUBLE)
+    amount = Column(DOUBLE)
+    vol = Column(DOUBLE)
+    count = Column(DOUBLE)
+
+
+class MDMin60FIllHistory(BaseModel):
+    __tablename__ = 'md_min60_bc_fill_history'
+    market = Column(String(10), primary_key=True)
+    pair = Column(String(10), primary_key=True)
+    ts_start = Column(TIMESTAMP, primary_key=True)
+    ts_curr = Column(TIMESTAMP)
+    open = Column(DOUBLE)
+    high = Column(DOUBLE)
+    low = Column(DOUBLE)
+    close = Column(DOUBLE)
+    amount = Column(DOUBLE)
+    vol = Column(DOUBLE)
+    count = Column(DOUBLE)
+
+
+class MDMinDaily(BaseModel):
+    __tablename__ = 'md_daily_bc'
+    market = Column(String(10), primary_key=True)
+    pair = Column(String(10), primary_key=True)
+    ts_start = Column(TIMESTAMP, primary_key=True)
+    ts_curr = Column(TIMESTAMP)
+    open = Column(DOUBLE)
+    high = Column(DOUBLE)
+    low = Column(DOUBLE)
+    close = Column(DOUBLE)
+    amount = Column(DOUBLE)
+    vol = Column(DOUBLE)
+    count = Column(DOUBLE)
+
+
+class MDMinDailyFIllHistory(BaseModel):
+    __tablename__ = 'md_daily_bc_fill_history'
+    market = Column(String(10), primary_key=True)
+    pair = Column(String(10), primary_key=True)
+    ts_start = Column(TIMESTAMP, primary_key=True)
+    ts_curr = Column(TIMESTAMP)
+    open = Column(DOUBLE)
+    high = Column(DOUBLE)
+    low = Column(DOUBLE)
+    close = Column(DOUBLE)
+    amount = Column(DOUBLE)
+    vol = Column(DOUBLE)
+    count = Column(DOUBLE)
+
+
 def init(alter_table=False):
     BaseModel.metadata.create_all(engine_md)
     if alter_table:
         with with_db_session(engine=engine_md) as session:
             for table_name, _ in BaseModel.metadata.tables.items():
                 sql_str = f"show table status from {Config.DB_SCHEMA_MD} where name=:table_name"
-                row_data = session.execute(sql_str, params={'table_name': 'md_min1_bc'}).first()
+                row_data = session.execute(sql_str, params={'table_name': table_name}).first()
                 if row_data is None:
                     continue
                 if row_data[1].lower() == 'myisam':
                     continue
 
-                logger.info('修改 %s 表引擎为 MyISAM')
+                logger.info('修改 %s 表引擎为 MyISAM', table_name)
                 sql_str = "ALTER TABLE %s ENGINE = MyISAM" % table_name
                 session.execute(sql_str)
 
             # This is an issue  https://www.mail-archive.com/sqlalchemy@googlegroups.com/msg19744.html
-            session.execute("ALTER TABLE md_min1_tick_bc CHANGE COLUMN `id` `id` INT(11) NULL AUTO_INCREMENT")
+            session.execute(f"ALTER TABLE {MDTick.__tablename__} CHANGE COLUMN `id` `id` INT(11) NULL AUTO_INCREMENT")
+            session.commit()
+
     logger.info("所有表结构建立完成")
 
 
